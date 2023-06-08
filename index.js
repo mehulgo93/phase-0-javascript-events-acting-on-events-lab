cf1466bcad2e92ab4aad826e68b8d3bbf90a93ae
Mehul <mehul_angle@hotmail.com>
2023-06-08 16:13:13 -0400

    JS Event

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const currentLeft = parseInt(dodger.style.left);
+    
+    if (currentLeft > 0) {
+      dodger.style.left = `${currentLeft - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const currentLeft = parseInt(dodger.style.left);
+    const containerWidth = document.getElementById('game').offsetWidth;
+    const dodgerWidth = dodger.offsetWidth;
+    
+    if (currentLeft + dodgerWidth >= containerWidth) {
+      dodger.style.left = `${currentLeft + 1}px`;
+    }
+  }
